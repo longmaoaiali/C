88324e8ee0442cd935bc358a1fe49ed6e6f257a0
liuyu7146 <liuyu7146@cvte.com>
2021-09-24 20:28:25 +0800

    modify plusOne question

--- Leetcode/数组/加一.c
+++ Leetcode/数组/加一.c
@@ -1,43 +1,24 @@
 int* plusOne(int* digits, int digitsSize, int* returnSize){
-    //判断最后一位是不是9
-    if(digits[digitsSize-1] != 9){
-        digits[digitsSize-1] += 1;
-        *returnSize = digitsSize;
-        return digits;
-    }
-
-    //遍历数组 找到 digits[i] != 9 或者 i=0
-    int i=digitsSize-1;
-    int j;
-    while(i>=0 && digits[i]==9){
-        i--;
-    }
-
-    if(i<0){
-        //999 --> 1000的情况
-        int size = digitsSize+1;
-        *returnSize = size;
-        digits = malloc(sizeof(int)*size);
-        digits[0]=1;
-        for(j=1;j<size;j++){
-            digits[j] = 0; 
+    int i;
+    for(i=digitsSize-1;i>=0;i--){
+        if(digits[i] == 9){
+            digits[i]=0;
+            if(i==0){
+                *returnSize = digitsSize+1;
+            }
+        } else {
+            digits[i] += 1;
+            *returnSize = digitsSize;
+            return digits;
         }
     }
 
-
-    if(i>=0 && digits[i] != 9)
-    {
-		//得考虑989 499
-        //499 --> 500
-        digits[i] += 1;
-        for(j=i+1;j<digitsSize;j++){
-            digits[j] = 0;
-        }
-
+    //数组拉长1
+    int *array = (int *)malloc(sizeof(int)*(*returnSize));
+    array[0]=1;
+    for(i=1;i<*returnSize;i++){
+        array[i]=0;
     }
 
-        
-    return digits;
-    
-
+    return array;
 }
